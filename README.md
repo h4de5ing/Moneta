4cbaf988f4524c2d557d497bbddc126768c9e050
h4de5ing <lex109@qq.com>
2017-02-28 15:41:28 +0800

    add GlideUtils

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Moneta 自定义View UI库
+
 # 对常用系统控件进行封装，实现调用更加简单
 
 ## How to use
 
 - com.code19.monetaui.widiget  自定义控件
+
  这个包里面的控件均可以直接在xml布局中使用
 
 * CountDownView 倒计时控件
@@ -19,6 +21,8 @@
 ```
 
 - com.code19.monetaui.utils  控件工具类
+
  这个包里面的类是直接在Activity或者Fragment中直接调用
 
 * DialogUtils 对话框相关的工具类
+* GlideUtils Glide图片请求封装工具类
---
+++ monetaui/src/main/java/com/code19/monetaui/utils/GlideUtils.java
@@ -0,0 +1,202 @@
+package com.code19.monetaui.utils;
+
+import android.content.Context;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.FutureTarget;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import java.io.File;
+
+
+public class GlideUtils {
+
+    /**
+     * 以下三个方法适合在onDestroy方法处调用
+     * 取消图片请求
+     *
+     * @param iv
+     */
+    public static void clearImageView(ImageView iv) {
+        Glide.clear(iv);
+    }
+
+    public static void clearImageView(FutureTarget target) {
+        Glide.clear(target);
+    }
+
+    public static void clearImageView(Target target) {
+        Glide.clear(target);
+    }
+
+    /**
+     * 显示圆角图片
+     *
+     * @param context
+     * @param url
+     * @param iv
+     */
+    public static void intoWithCircle(Context context, String url, ImageView iv) {
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).centerCrop().transform(new GlideCircleTransform(context)).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(iv);
+        } else {
+            Glide.with(context).load(url).centerCrop().transform(new GlideCircleTransform(context)).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(iv);
+        }
+    }
+
+    /**
+     * 显示圆角图片
+     *
+     * @param context
+     * @param file
+     * @param iv
+     */
+    public static void intoWithCircle(Context context, File file, ImageView iv) {
+        Glide.with(context).load(file).centerCrop().transform(new GlideCircleTransform(context)).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(iv);
+    }
+
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param url     可以是本地路径或者网络图片地址
+     * @param iv
+     */
+    public static void into(Context context, String url, ImageView iv) {
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).into(iv);
+        } else {
+            Glide.with(context).load(url).into(iv);
+        }
+    }
+
+    public static void into(Context context, String url, ImageView iv, int error) {
+        if (url == null) {
+            url = "";
+        }
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).error(error).into(iv);
+        } else {
+            Glide.with(context).load(url).error(error).into(iv);
+        }
+    }
+
+    public static void into(Context context, int url, ImageView iv) {
+        Glide.with(context).load(url).into(iv);
+    }
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param url        可以是本地路径或者网络图片地址
+     * @param iv
+     * @param placehoder
+     */
+    public static void intoCenterCrop(Context context, String url, ImageView iv, int placehoder) {
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).centerCrop().into(iv);
+        } else {
+            Glide.with(context).load(url).centerCrop().into(iv);
+        }
+    }
+
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param url     可以是本地路径或者网络图片地址
+     * @param iv
+     */
+    public static void intoCenterCrop(Context context, String url, ImageView iv, RequestListener loadListener) {
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).asBitmap().centerCrop().into(iv);
+        } else {
+            Glide.with(context).load(url).asBitmap().centerCrop().listener(loadListener).into(iv);
+        }
+    }
+
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param url
+     * @param iv
+     */
+    public static void intoCenterCrop(Context context, String url, ImageView iv) {
+        Glide.with(context).load(url).centerCrop().into(iv);
+    }
+
+    public static void intoCenterCropForUrl(Context context, String url, ImageView iv, RequestListener loadListener) {
+        Glide.with(context).load(url).centerCrop().listener(loadListener).into(iv);
+    }
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param url
+     * @param iv
+     */
+    public static void intoDefault(Context context, String url, ImageView iv) {
+        Glide.with(context).load(url).into(iv);
+    }
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param uri
+     * @param iv
+     */
+    public static void intoCenterCrop(Context context, Uri uri, ImageView iv) {
+        Glide.with(context).load(uri).centerCrop().into(iv);
+    }
+
+    /**
+     * 显示非圆角图片
+     *
+     * @param context
+     * @param url
+     * @param iv
+     */
+    public static void intoFit(Context context, String url, ImageView iv) {
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).asBitmap().fitCenter().into(iv);
+        } else {
+            Glide.with(context).load(url).asBitmap().fitCenter().into(iv);
+        }
+    }
+
+
+    public static void intoWithCircle(Context context, String url, ImageView iv, RequestListener loadListener) {
+        File file = new File(url);
+        if (file.exists()) {
+            Glide.with(context).load(file).asBitmap().centerCrop().transform(new GlideCircleTransform(context)).listener(loadListener).into(iv);
+        } else {
+            Glide.with(context).load(url).centerCrop().transform(new GlideCircleTransform(context)).listener(loadListener).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(iv);
+        }
+    }
+
+    public static void intoCenterCrop(Context context, String url, ImageView iv, Transformation Transformation) {
+        Glide.with(context).load(url).bitmapTransform(Transformation).into(iv);
+    }
+
+    public static void intoCenterCrop(Context context, String url, ImageView iv, Transformation... Transformation) {
+        Glide.with(context).load(url).bitmapTransform(Transformation).into(iv);
+    }
+}
